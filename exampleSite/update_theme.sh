91c6bf92f9c3d1ee3880e69a76e235aa9275eeeb
speeedops <speedoops@live.com>
2020-09-11 11:26:30 +0800

    change to speedoops

--- exampleSite/update_theme.sh
+++ exampleSite/update_theme.sh
@@ -7,13 +7,14 @@ fi
 
 if [ -d "themes/hugo-material-docs" ];then
   echo 'Find directoy "themes/hugo-material-docs", update by "git pull"'
-  cd themes/hugo-material-docs
+  pushd themes/hugo-material-docs
   git pull
-  cd ../../
+  popd
 else
   echo 'Directoy "themes/hugo-material-docs" not found, do "git clone"'
-  git clone https://github.com/skyao/hugo-material-docs.git themes/hugo-material-docs
+  #git clone https://github.com/skyao/hugo-material-docs.git themes/hugo-material-docs
   #git clone git@github.com:skyao/hugo-material-docs.git themes/hugo-material-docs
+  git clone https://github.com/speedoops/hugo-material-docs.git themes/hugo-material-docs
   #cd themes
   #ln -s ../../hugo-material-docs .
 fi
